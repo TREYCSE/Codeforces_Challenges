afd95cd24da7d11a4edabe5186350f740da0497d
trey_weathers <treychelle.weathers2017@gmail.com>
2023-09-30 06:18:24 -0400

    evil.py

---
+++ evil.py
@@ -0,0 +1,8 @@
+# Input - The first line of the input gives the number of test cases, T.
+# T test cases follow.
+
+
+
+# For each test case, the first line contains an integer N, where N is the number of competitions. The next line contains N integers a1, a2, . . . , aN representing the number of competitors in each competition.
+
+# Output - For each test case, output one line containing “Case #x: y”, where x is the test case number (starting from 1) and y is the minimum number of sketchpads that should be produced by Tiger’s factory.
