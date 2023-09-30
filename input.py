e3570219c85edfb3a60afa007105b662e998b7e6
trey_weathers <treychelle.weathers2017@gmail.com>
2023-09-30 11:56:33 -0400

    input.py alternative

---
+++ input.py
@@ -0,0 +1,37 @@
+# import sys (allows to read lines) and math from Python
+import math
+import numpy as np
+
+
+def compute_sketchpads(): 
+    # For each test case, the first line contains an integer N, where N is the number of competitions. The next line contains N integers a1, a2, . . . , aN representing the number of competitors in each competition.
+    # The first line of the input gives the number of test cases, T. # T test cases follow.
+    test_cases = 1
+    # loop over test cases
+    for x in range(int(test_cases)):
+        competitions = 6
+        competitors = np.array([13, 11, 11, 11, 13, 11])
+        y = 0
+        
+        # loop over competitions
+        for i in range(competitions):
+            # integer / index competitors defined above
+            sketch_pads = int(competitors[i])
+            # multiply original pads by the additional 10% (0.1)
+            extra_pads = sketch_pads * 0.1
+            # add the additional pads to the original set
+            y += sketch_pads + extra_pads
+        
+        # Output - For each test case, output one line containing “Case #x: y”, where x is the test case number (starting from 1) and y is the minimum number of sketchpads that should be produced by Tiger’s factory.
+        print(f"Code: #{x+1}:{math.ceil(y)}")
+compute_sketchpads()
+
+#test input values
+# 1
+# 6
+# 13, 11, 11, 11, 13, 11
+
+# test output
+# Case #1: 82
+
+   
